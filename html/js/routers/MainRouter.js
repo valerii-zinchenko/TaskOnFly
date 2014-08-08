01f6501ed98b3254a2a05f3678ddf5384c0c3a0d
valerii-zinchenko <zinchenko.valerii@gmail.com>
2014-08-08 19:30:48 +0300

    Upgrade license to GPLv3

--- html/js/routers/MainRouter.js
+++ html/js/routers/MainRouter.js
@@ -1,6 +1,25 @@
-/**
- * Created by Valerii Zinchenko on 7/18/14.
- */
+/*
+ TaskOnFly. Manage your tasks and task lists on the fly.
+ Copyright (C) 2014  Valerii Zinchenko
+
+ This file is part of TaskOnFly.
+
+ TaskOnFly is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TaskOnFly is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TaskOnFly.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ All source files are available at: http://github.com/valerii-zinchenko/TaskOnFly
+*/
 
 'use strict';
 
