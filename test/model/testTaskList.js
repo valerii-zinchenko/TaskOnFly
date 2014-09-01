124f11b3cd5a38c28acc059c10b237b4a92d93b0
valerii-zinchenko <zinchenko.valerii@gmail.com>
2014-09-01 22:08:05 +0300

    #20 TaskList is fully covered by tests

--- test/model/testTaskList.js
+++ test/model/testTaskList.js
@@ -121,9 +121,177 @@ suite('Test TaskList', function() {
         assert.equal(TaskOnFly.getCurrentList(), List);
     });
 
-    test('_object2Array()', function() {});
+    test('_object2Array()', function() {
+        var obj = {
+            1: 1,
+            2: 2,
+            3: 3
+        };
 
-    test('sort()', function() {});
+        var result = Module.prototype._object2Array(obj, [3,2,1]);
 
-    test('filter()', function() {});
+        assert.isArray(result);
+        assert.deepEqual(result, [3,2,1]);
+    });
+
+    test('sort()', function() {
+        var objs = [
+            {
+                id: '0',
+                isDone: false,
+                dueDate: '2014-08-30',
+                priority: 0
+            },
+            {
+                id: '1',
+                isDone: false,
+                dueDate: '2014-08-31',
+                priority: 2
+            },
+            {
+                id: '2',
+                isDone: false,
+                dueDate: '2014-08-31',
+                priority: 1
+            },
+            {
+                id: '3',
+                isDone: false,
+                dueDate: '2014-08-31',
+                priority: 0
+            },            {
+                id: '4',
+                isDone: false,
+                dueDate: '2014-08-31',
+                priority: 0
+            },
+            {
+                id: '5',
+                isDone: true,
+                dueDate: '2014-09-01',
+                priority: 2
+            },
+            {
+                id: '6',
+                isDone: true,
+                dueDate: '2014-09-01',
+                priority: 0
+            },
+            {
+                id: '7',
+                isDone: true,
+                dueDate: '2014-09-02',
+                priority: 1
+            }
+        ];
+
+        List.addTask(objs[7]);
+        List.addTask(objs[1]);
+        List.addTask(objs[5]);
+        List.addTask(objs[2]);
+        List.addTask(objs[6]);
+        List.addTask(objs[4]);
+        List.addTask(objs[0]);
+        List.addTask(objs[3]);
+
+        var items = List.public.items;
+
+        assert.equal(items.length, objs.length, 'Count of expected task is not equal to the count of added tasks');
+
+        for (var n = 0, N = objs.length; n < N; n++) {
+            assert.equal(List.models[items[n]].public.id, objs[n].id, n + ': incorrect id property');
+            assert.equal(List.models[items[n]].public.isDone, objs[n].isDone, n + ': incorrect isDone property');
+            assert.equal(List.models[items[n]].public.dueDate, objs[n].dueDate, n + ': incorrect dueDate property');
+            assert.equal(List.models[items[n]].public.priority, objs[n].priority, n + ': incorrect priority property');
+        }
+    });
+
+    suite('filter()', function() {
+        setup(function(done) {
+            requirejs(['model/TaskList'], function(TaskList) {
+                Module = TaskList;
+                List = new Module('root');
+
+                List.addTask({
+                    id: '0',
+                    title: 'title',
+                    isDone: true,
+                    dueDate: '2014-08-31'
+                });
+                List.addTask({
+                    id: '1',
+                    title: 'task',
+                    isDone: false,
+                    dueDate: '2014-08-31'
+                });
+                List.addTask({
+                    id: '2',
+                    title: 'another title',
+                    isDone: false,
+                    dueDate: '2014-09-01'
+                });
+
+                done();
+            })
+        });
+
+        test('filter by title', function() {
+            var res = List.filter({
+                title: 'itl'
+            });
+
+            assert.equal(res.public.items.length, 2, 'Incorrect count of founded items');
+            assert.property(res.models, '0', 'no item with ID 0');
+            assert.property(res.models, '2', 'no item with ID 2');
+        });
+
+        test('filter by completeness', function() {
+            var res = List.filter({
+                isDone: true
+            });
+
+            assert.equal(res.public.items.length, 1, 'Incorrect count of founded items');
+            assert.property(res.models, '0', 'no item with ID 0');
+        });
+
+        test('filter by due date', function() {
+            var res = List.filter({
+                dueDate: '2014-08'
+            });
+
+            assert.equal(res.public.items.length, 2, 'Incorrect count of founded items');
+            assert.property(res.models, '0', 'no item with ID 0');
+            assert.property(res.models, '1', 'no item with ID 2');
+        });
+
+        test('filter by title and completeness', function() {
+            var res = List.filter({
+                title: 'itl',
+                isDone: false
+            });
+
+            assert.equal(res.public.items.length, 1, 'Incorrect count of founded items');
+            assert.property(res.models, '2', 'no item with ID 2');
+        });
+
+        test('filter by title and due date', function() {
+            var res = List.filter({
+                title: 'itl',
+                dueDate: '2014-08-31'
+            });
+
+            assert.equal(res.public.items.length, 1, 'Incorrect count of founded items');
+            assert.property(res.models, '0', 'no item with ID 0');
+        });
+
+        test('filter by completeness and due date', function() {
+            var res = List.filter({
+                isDone: false,
+                dueDate: '2014-09'
+            });
+
+            assert.equal(res.public.items.length, 1, 'Incorrect count of founded items');
+            assert.property(res.models, '2', 'no item with ID 2');
+        });
+    });
 });
