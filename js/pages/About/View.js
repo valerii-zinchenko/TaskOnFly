a6f7a70d47f39c4ffa028bd4375f73d7fcd02827
valerii-zinchenko <zinchenko.valerii@gmail.com>
2014-10-29 23:47:44 +0200

    add justify style to the text in About page

--- js/pages/About/View.js
+++ js/pages/About/View.js
@@ -34,13 +34,13 @@ define(function () {
 </div> \
 <div data-role="content"> \
     <div class="ui-body ui-body-a ui-corner-all"> \
-        <p>Version <%= TaskManager.version %></p> \
-        <p>TaskOnFly allows you easy manage your tasks and task lists on the fly from your mobile or desktop device.</p> \
-        <p>Copyright (C) 2014  Valerii Zinchenko</p> \
-        <p>TaskOnFly is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p> \
-        <p>TaskOnFly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.</p> \
-        <p>You should have received a copy of the GNU General Public License along with TaskOnFly.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p> \
-        <p>All source files are available at: <a href="http://github.com/valerii-zinchenko/TaskOnFly">http://github.com/valerii-zinchenko/TaskOnFly</a></p> \
+        <p class="justify">Version <%= TaskManager.version %></p> \
+        <p class="justify">TaskOnFly allows you easy manage your tasks and task lists on the fly from your mobile or desktop device.</p> \
+        <p class="justify">Copyright (C) 2014  Valerii Zinchenko</p> \
+        <p class="justify">TaskOnFly is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p> \
+        <p class="justify">TaskOnFly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.</p> \
+        <p class="justify">You should have received a copy of the GNU General Public License along with TaskOnFly.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p> \
+        <p class="justify">All source files are available at: <a href="http://github.com/valerii-zinchenko/TaskOnFly">http://github.com/valerii-zinchenko/TaskOnFly</a></p> \
     </div> \
 </div>',
 
