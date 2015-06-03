9fd8e9d45a9cad89891b3a29d1d6cb262816e5d8
valerii-zinchenko <zinchenko.valerii@gmail.com>
2015-06-03 21:10:37 +0300

    Configure testing and code coverage (istanbul) tools

---
+++ test/testRunner.js
@@ -0,0 +1,25 @@
+window.assert = chai.assert;
+window.sinon = sinon;
+
+mocha.setup({
+	ui: 'tdd',
+	bail: false
+});
+
+requirejs([
+	'utils',
+	'aclass'
+], function() {
+	requirejs([
+		'class',
+		'singleton',
+		//'astate',
+		'./core/test_utils.js',
+		'./core/testAClass.js',
+		'./core/testClass.js',
+		'./core/testSingletonClass.js'
+		//'./core/testMVCModule.js'
+	], function() {
+			mocha.run();
+	});
+});
